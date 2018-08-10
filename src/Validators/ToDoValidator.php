901773d6725773475f6cfe68fdb95428912222a8
External DEV <daniel.borza@plentydevelopment.com>
2018-08-10 15:17:55 +0300

    a

---
+++ src/Validators/ToDoValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace ToDoList\Validators;
+
+use Plenty\Validation\Validator;
+
+/**
+ *  Validator Class
+ */
+class ToDoValidator extends Validator
+{
+    protected function defineAttributes()
+    {
+        $this->addString('taskDescription', true);
+    }
+}
